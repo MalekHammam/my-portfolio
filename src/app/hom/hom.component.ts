a7a54d5a9571e0de21130c3c4821848638c9ce4b
malak <malakhammami263@gmail.com>
2022-12-07 22:24:05 +0100

    intial commit for the project

--- src/app/hom/hom.component.ts
+++ src/app/hom/hom.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Inject, Input, OnInit, Renderer2 } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { DOCUMENT } from '@angular/common';
 
 import { MatDialog } from '@angular/material/dialog';
 
@@ -11,15 +12,37 @@ import { OverlayContainer } from '@angular/cdk/overlay';
 })
 
 export class HomComponent implements OnInit {
+  theme: Theme = 'light-theme';
   
   constructor(
+    @Inject(DOCUMENT) private document: Document,
+     private renderer: Renderer2
 ) { }
   
 
   ngOnInit(): void {
+    this.initializeTheme();
   }
- 
+  switchTheme() {
+    this.document.body.classList.replace(
+      this.theme,
+      this.theme === 'light-theme'
+        ? (this.theme = 'dark-theme')
+        : (this.theme = 'light-theme')
+    );
+  }
+
+  initializeTheme = (): void =>
+    this.renderer.addClass(this.document.body, this.theme);
 }
+document.body
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg";
+
+
+export type Theme = 'light-theme' | 'dark-theme';
+
+
 
 
 
