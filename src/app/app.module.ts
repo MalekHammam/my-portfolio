2e75bf83737587960f59f3dc3a21b3e26f756177
MalekHammam <hammamiimalek@outlook.com>
2022-12-12 15:32:56 +0100

    Adds Github Actions workflow to deploy Github Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomComponent } from './hom/hom.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
+
 import { ResumeComponent } from './resume/resume.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -16,24 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({
-      "radius": 60,
-      "space": -10,
-      "outerStrokeGradient": true,
-      "outerStrokeWidth": 10,
-      "outerStrokeColor": "#4882c2",
-      "outerStrokeGradientStopColor": "#53a9ff",
-      "innerStrokeColor": "#e7e8ea",
-      "innerStrokeWidth": 10,
-      "title": "UI",
-      "animateTitle": false,
-      "animationDuration": 1000,
-      "showUnits": false,
-      "showBackground": false,
-      "clockwise": false,
-      "startFromZero": false,
-      "lazy": true}),
-    BrowserAnimationsModule
+
+  
    
   ],
   providers: [],
